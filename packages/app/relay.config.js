b276d9de8b24218634971ece848d0596d1b36c87
Pedro Arantes <arantespp@gmail.com>
2020-10-07 10:23:17 -0300

    feat: first app run

--- packages/app/relay.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  artifactDirectory: './src/__generated__',
-  language: 'typescript',
-  src: '.',
-  schema: '../appsync/src/schema.graphql',
-  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
-};
